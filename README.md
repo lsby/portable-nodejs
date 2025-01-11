8120f8a6e94d8802cac573bebff49759e4f5728e
hbybyyang <2451759073@qq.com>
2025-01-11 21:22:18 +0800

    实现功能

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # portable-nodejs
+
+在当前目录生成全功能, 便携式的 nodejs.
+
+可以运行 npm.
+
+所谓便携式, 是指可以将其拷贝到任何计算机的任何路径下, 都可以正常使用.
+
+只支持 windows.
+
+## 使用
+
+在你希望创建环境的目录用 PowerShell 执行:
+
+```
+irm "https://raw.githubusercontent.com/lsby/portable-nodejs/main/init.ps1" | iex
+```
+
+## 细节
+
+nodejs 官方提供了独立二进制文件分发, 本仓库提供的 zip 文件为官方文件的副本.
---
+++ init.ps1
@@ -0,0 +1,56 @@
+$ErrorActionPreference = "Stop"
+
+# 获取选项
+Write-Output "获取可选择的版本..."
+$url = "https://github.com/lsby/portable-nodejs/releases/expanded_assets/nodejs"
+$response = Invoke-WebRequest -Uri $url
+$htmlContent = $response.Content
+$pattern = 'node-(.*?)-win-x64.zip'
+$matches = [regex]::Matches($htmlContent, $pattern)
+$uniqueMatches = @{}
+foreach ($match in $matches) {
+    $uniqueMatches[$match.Value] = $true
+}
+
+# 用户选择
+$selectedMatch = $uniqueMatches.Keys | Out-GridView -Title "Select a match" -OutputMode Single
+if ([string]::IsNullOrEmpty($selectedMatch)) {
+    Write-Output "用户没有选择任何版本, 脚本将退出..."
+    exit
+}
+Write-Output "用户选择了: $selectedMatch"
+
+# 下载
+Write-Output "开始下载..."
+$downloadUrl = "https://github.com/lsby/portable-nodejs/releases/download/nodejs/$selectedMatch"
+$localFileName = "lsby-portable-nodejs.zip"
+Invoke-WebRequest -Uri $downloadUrl -OutFile $localFileName
+Write-Output "下载完成..."
+
+# 解压缩
+Write-Output "开始解压..."
+$extractedFolder = [System.IO.Path]::GetFileNameWithoutExtension($localFileName)
+Expand-Archive -Path $localFileName -DestinationPath $extractedFolder -Force
+Write-Output "解压完成..."
+
+# 删除压缩包
+Write-Output "删除原始压缩包..."
+Remove-Item -Path $localFileName -Force -ErrorAction SilentlyContinue
+
+# 调整结构
+Write-Output "调整结构..."
+$folderName = $selectedMatch -replace '\.zip$', ''
+$nodeFolder = Join-Path $extractedFolder $folderName
+Move-Item -Path $nodeFolder\* -Destination $extractedFolder -Force
+Remove-Item -Path $nodeFolder -Force -Recurse
+
+# 创建进入环境文件
+$scriptContent = @"
+@echo off
+start cmd /k "set PATH=%CD%\lsby-portable-nodejs;%PATH%"
+"@
+$scriptPath = $PWD.Path
+$scriptFilePath = Join-Path $scriptPath "lsby-portable-nodejs-进入nodejs环境.cmd"
+[System.IO.File]::WriteAllLines($scriptFilePath, $scriptContent)
+
+Write-Output "完成"
